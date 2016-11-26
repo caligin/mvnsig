754205d20fd41beab81487ca1af732f66087fb69
Tsukihara Caligin <caligin35@gmail.com>
2016-11-26 19:23:43 +0000

    ref: pulled out variables, parsable output

--- mvnsig.sh
+++ mvnsig.sh
@@ -1,32 +1,35 @@
 REPO_BASE=https://repo1.maven.org/maven2/
 LOCAL_REPO_BASE=${HOME}/.m2/repository/
+KEYSERVER=pgp.mit.edu
 for jar in $(find ${LOCAL_REPO_BASE} -name '*.jar' | cut -d / -f 6-); do
   asc="${jar}.asc"
+  jar_abs=${LOCAL_REPO_BASE}${jar}
+  asc_abs=${LOCAL_REPO_BASE}${asc}
   if [ ! -f "${LOCAL_REPO_BASE}${asc}" ]; then
-    http_status=$(curl "${REPO_BASE}${asc}" -s -o ${LOCAL_REPO_BASE}${asc}  -w "%{http_code}")
+    http_status=$(curl "${REPO_BASE}${asc}" -s -o ${asc_abs}  -w "%{http_code}")
     if [ "${http_status}" == 404 ]; then
-      echo "FAIL: signature not found"
-      rm -f "${LOCAL_REPO_BASE}${asc}"
+      echo "FAIL NOSIG ${jar_abs} NOKEY signature not found in remote repo"
+      rm -f "${asc_abs}"
       continue
     fi
     if [ ! "${http_status}" == 200 ]; then
-      echo "FAIL: signature fetch failed with status ${http_status}"
-      rm -f "${LOCAL_REPO_BASE}${asc}"
+      echo "FAIL ERROR ${jar_abs} NOKEY signature fetch failed with status ${http_status}"
+      rm -f "${asc_abs}"
       continue
     fi
   fi
-  key_id=$(gpg2 -vv "${LOCAL_REPO_BASE}${asc}" 2>&1 | grep -E 'using \S+ key' | rev | cut -d ' ' -f1 | rev)
-  gpg2 -q --keyserver pgp.mit.edu --recv-keys "${key_id}" &> /dev/null
+  key_id=$(gpg2 -vv "${asc_abs}" 2>&1 | grep -E 'using \S+ key' | rev | cut -d ' ' -f1 | rev)
+  gpg2 -q --keyserver ${KEYSERVER} --recv-keys "${key_id}" &> /dev/null
   if [ $? -eq 0 ]; then
-    gpg2 -q --verify "${LOCAL_REPO_BASE}${asc}" &> /dev/null
+    gpg2 -q --verify "${asc_abs}" &> /dev/null
     if [ $? -eq 0 ]; then
-      echo "OK: good sign for ${LOCAL_REPO_BASE}${jar}"
+      echo "OK GOOD ${jar_abs} ${key_id}"
     else
-      echo "FAIL: bad sign for ${LOCAL_REPO_BASE}${jar}"
+      echo "FAIL BAD ${jar_abs} ${key_id}"
       continue
     fi
   else
-    echo "FAIL: cannot find key ${key_id} on keyserver"
+    echo "FAIL NOKEY ${jar_abs} ${key_id} cannot find key on keyserver ${KEYSERVER}"
     continue
   fi
 done
